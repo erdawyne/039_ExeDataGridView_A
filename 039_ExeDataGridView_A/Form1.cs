b32d4d22d05289ac1bb635499a0049655abbf9a1
erdawyne <erdawynee1@gmail.com>
2022-04-06 17:38:32 +0700

    Mengubah warna background

--- 039_ExeDataGridView_A/Form1.cs
+++ 039_ExeDataGridView_A/Form1.cs
@@ -30,9 +30,9 @@ namespace _039_ExeDataGridView_A
             this.dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.DarkBlue;
 
             //Buat Background
-            this.dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+            this.dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Salmon;
             this.dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
-            this.dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+            this.dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Yellow;
             this.dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.DarkBlue;
             this.dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Crimson;
 
